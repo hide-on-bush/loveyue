de8bdb1be3eed56c5d2035c015eff7d0c1e2ebf4
xiashixiong <xiashixiong@xgqq.com>
2018-07-10 17:08:56 +0800

    update 7

--- 第七个/1/jscss/time.js
+++ 第七个/1/jscss/time.js
@@ -1,4 +1,4 @@
-function showTime() { 
+﻿function showTime() { 
 	var currentDate = new Date(); 
 	var startDate = new Date(2014, 12, 01);
 	var date3 = currentDate-startDate;
@@ -13,7 +13,7 @@ function showTime() {
 	minutes = "0" + minutes; 
 	if (seconds < 10) 
 	seconds = "0" + seconds; 
-	var currentTimeString = "Dear Yue:<br>"
+	var currentTimeString = "Dear Huihui:<br>"
 		+ "I have been in love with you for:<br> "
 		+ "&nbsp&nbsp&nbsp&nbsp&nbsp"
 		+ "<c style=\"color: #CCFF99; text-shadow:2px 3px 3px #222; font-family:Microsoft YaHei; font-size:24px\" >" + days +" </c>day " 
